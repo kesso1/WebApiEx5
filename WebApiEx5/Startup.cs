c3efd76d45c6c77e9b71719a6b1ac970911376dc
Othmar Kesseli <othmar.kesseli@outlook.com>
2018-03-30 14:32:24 +0200

    Projektdateien hinzufügen.

---
+++ WebApiEx5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApiEx5.Startup))]
+namespace WebApiEx5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
